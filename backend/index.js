34161a57c85598c7c045542afcb101ae55ea2a5b
mgonline86 <mgonline86@yahoo.com>
2022-12-08 17:24:53 +0200

    frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 dotenv.config()
+import cors from 'cors'
 import mongoose  from 'mongoose'
 import todoRouter from './src/routes/TodoRoutes.js'
 
@@ -9,7 +10,7 @@ const PORT=process.env.PORT || 3000
 
 // middleware
 app.use(express.json())
-
+app.use(cors())
 
 app.use('/api',todoRouter)
 
---
+++ todo/src/pages/Create.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { useState } from "react";
+import axios from 'axios'
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import { Button } from "@mui/material";
+
+export default function SimpleContainer() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, desc);
+
+    if ((title, desc)) {
+        axios.post('http://localhost:4000/api/1', {
+            title, desc
+        }).then((res) => { console.log(res) })
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ width: "100%", maxWidth: 500 }}>
+          <form onSubmit={handleSubmit}>
+            <Typography variant="h5" gutterBottom>
+              ToDo List
+            </Typography>
+            <TextField
+              onChange={(e) => setTitle(e.target.value)}
+              required
+              id="outlined-required"
+              label="Title"
+              sx={{
+                width: 500,
+                maxWidth: "100%",
+              }}
+            />
+            <TextareaAutosize
+              onChange={(e) => setDesc(e.target.value)}
+              aria-label="minimum height"
+              minRows={10}
+              placeholder="Desciption"
+              style={{
+                width: 500,
+                maxWidth: "100%",
+                marginTop: "20PX",
+              }}
+            />
+            <Button type="submit" color="secondary" variant="contained">
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
